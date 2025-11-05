996106f1cd65011008976dfd86d76b9358cfa95f
Daniel Romo <danielromoneri@gmail.com>
2025-11-04 22:03:55 -0600

    Se avanza lógica para generar orden de cartas aleatorio

--- ViewModels/QuizGameVM.cs
+++ ViewModels/QuizGameVM.cs
@@ -13,16 +13,40 @@ namespace FastQuizMAUI.ViewModels
     public partial class QuizGameVM : ObservableObject
     {
         private readonly DatabaseService _databaseService;
-
+        private readonly Random random = new Random();
+        private List<int> indexLeft = new List<int>();
         public QuizGameVM(DatabaseService databaseService)
         {
             _databaseService = databaseService;
         }
+
+        public void SetIndexLeft()
+        {
+            for (int i = 0; i < ItemsToQuiz.Length; i++)
+            {
+                indexLeft.Add(i);
+            }
+        }
+
         [ObservableProperty]
         private ItemsBoxModel[] _itemsToQuiz;
 
         [ObservableProperty]
         private ItemsBoxModel _currentItem;
 
+
+        private void SetNewCard()
+        {
+            if(indexLeft.Count > 0)
+            {
+                int randomIndexFromList = random.Next(0, indexLeft.Count);
+
+                int indexItem = indexLeft[randomIndexFromList];
+
+                CurrentItem = ItemsToQuiz[indexItem];
+                indexLeft.RemoveAt(randomIndexFromList);
+            }
+
+        }
     }
 }
